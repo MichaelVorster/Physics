da541f65a78c8ecbb5eac6dbc73a9227d94dba3b
Michael Vorster <michael.j.vorster@gmail.com>
2019-04-24 17:27:27 +0200

    Pre-format commit

--- delta_B_B.f08
+++ delta_B_B.f08
@@ -10,13 +10,13 @@ program main
 ! **                                       **
 ! *******************************************
 
-  integer :: time = 12
-  character(len=350) :: file_dir = '/home/cronus/vorster/512_data/bx1.6_cs0.8/initial_turbulence_for_PLUTO/'
+  integer :: time = 14
+  character(len=350) :: file_dir = '/home/cronus/vorster/Dust_grains/Kirit_data/' 
   logical :: shock_present = .false.
-  character(len=1) :: shock_direction = 'x'
-  integer :: avg_shock_pos = 266
+  character(len=1) :: shock_direction = 'y'
+  integer :: avg_shock_pos = 252
 ! 'cut': number of cells on either side of average shock position that is be excluded
-  integer :: cut = 0
+  integer :: cut = 10
 
 ! *******************************************
 ! **                                       **
@@ -25,12 +25,15 @@ program main
 ! *******************************************
 
   integer, save :: nx, ny, nz
-  real(dp), dimension(:,:,:), allocatable :: magx, magy, magz
-  real(dp), dimension(:,:,:), allocatable :: mag_temp
+  real(dp), dimension(:,:,:), allocatable :: Bx, By, Bz
+  real(dp), dimension(:,:,:), allocatable :: B_temp
+  real(dp), dimension(:,:,:), allocatable :: B_mag
   integer :: temp_nx, temp_ny, temp_nz
   real(dp) :: B_avg_component(3), delta_B_component(3)
   real(dp) :: component_temp
   real(dp) :: avg_B, delta_B
+  real(dp) :: mu_dispersion
+  real(dp) :: temp_val
 
   character(len=350) :: fname
   character(len=10) :: tmp
@@ -42,45 +45,53 @@ program main
   read(9) nx, ny, nz
   close(9)
 
-  allocate (magx(nx,ny,nz))
-  allocate (magy(nx,ny,nz))
-  allocate (magz(nx,ny,nz))
-  allocate (mag_temp(nx,ny,nz))
+  allocate (Bx(nx,ny,nz))
+  allocate (By(nx,ny,nz))
+  allocate (Bz(nx,ny,nz))
+  allocate (B_temp(nx,ny,nz))
+  allocate (B_mag(nx,ny,nz))
 
   ! read turbulence data
   write (tmp, '(I3,A4)') time, '.BIN'
   fname = trim(file_dir) // '/BB' // adjustl(tmp)
   open(unit=iu, file=trim(fname), form='unformatted', status='old',  action='read')
     read(iu) temp_nx, temp_ny, temp_nz
-    read(iu) magx(:,:,:)
-    read(iu) magy(:,:,:)
-    read(iu) magz(:,:,:)
+    read(iu) Bx(:,:,:)
+    read(iu) By(:,:,:)
+    read(iu) Bz(:,:,:)
   close(iu)
 
   ! calculate average B
   if (shock_present .eqv. .true.) then
-    call cut_average(B_avg_component(1), magx)
-    call cut_average(B_avg_component(2), magy)
-    call cut_average(B_avg_component(3), magz)
+    call cut_average(B_avg_component(1), Bx)
+    call cut_average(B_avg_component(2), By)
+    call cut_average(B_avg_component(3), Bz)
   else
-    B_avg_component(1) = sum(magx)/(nx*ny*nz)
-    B_avg_component(2) = sum(magy)/(nx*ny*nz)
-    B_avg_component(3) = sum(magz)/(nx*ny*nz)
+    B_avg_component(1) = sum(Bx)/(nx*ny*nz)
+    B_avg_component(2) = sum(By)/(nx*ny*nz)
+    B_avg_component(3) = sum(Bz)/(nx*ny*nz)
   endif
   avg_B = sqrt(sum(B_avg_component**2))
+
+  ! calculate magnitude of B at every point
+  B_mag = sqrt(Bx**2 + By**2 + Bz**2)
+
+  ! calculate (delta v_para)/v_perp  : Equation C6 in Hoang, Lazarain, Schlickeiser (2012), ApJ 747:54
+  temp_val = sum((B_mag - avg_B)**2)/(nx*ny*nz)
+  mu_dispersion = temp_val**(1./4.)/avg_B**(1./2.)
 
   print*, ''
   print('(A, 3ES14.2)'), 'Average B components:', B_avg_component
   print('(A, ES14.2)'), 'Average B:', avg_B
 
   if (shock_present .eqv. .true.) then
-    call cut_delta_component(delta_B_component(1), B_avg_component(1), magx)
-    call cut_delta_component(delta_B_component(2), B_avg_component(2), magy)
-    call cut_delta_component(delta_B_component(3), B_avg_component(3), magz)
+    call cut_delta_component(delta_B_component(1), B_avg_component(1), Bx)
+    call cut_delta_component(delta_B_component(2), B_avg_component(2), By)
+    call cut_delta_component(delta_B_component(3), B_avg_component(3), Bz)
   else
-    delta_B_component(1) = sqrt(sum(magx**2)/(nx*ny*nz) - B_avg_component(1)**2)
-    delta_B_component(2) = sqrt(sum(magy**2)/(nx*ny*nz) - B_avg_component(2)**2)
-    delta_B_component(3) = sqrt(sum(magz**2)/(nx*ny*nz) - B_avg_component(3)**2)
+    delta_B_component(1) = sqrt(sum(Bx**2)/(nx*ny*nz) - B_avg_component(1)**2)
+    delta_B_component(2) = sqrt(sum(By**2)/(nx*ny*nz) - B_avg_component(2)**2)
+    delta_B_component(3) = sqrt(sum(Bz**2)/(nx*ny*nz) - B_avg_component(3)**2)
   endif
   delta_B = sqrt(sum(delta_B_component**2))
 
@@ -90,6 +101,10 @@ program main
 
   print*, ''
   print('(A, ES14.2)'), 'delta_B/B', delta_B/avg_B
+  print*, ''
+
+  print*, ''
+  print('(A, ES14.2)'), 'mu dispersion', mu_dispersion
   print*, ''
 
   fname = trim(file_dir) // '/delta_B_B.txt'
@@ -103,67 +118,68 @@ program main
     write(iu,'(A, ES14.2)') 'delta_B/B', delta_B/avg_B
   close(iu)
   
-  deallocate (magx)
-  deallocate (magy)
-  deallocate (magz)
-  deallocate (mag_temp)
+  deallocate (Bx)
+  deallocate (By)
+  deallocate (Bz)
+  deallocate (B_temp)
+  deallocate (B_mag)
 
 
 contains
 
 
-subroutine make_cut_x(component, mag)
+subroutine make_cut_x(component, B)
   real(dp), intent(inout) :: component
-  real(dp), intent(inout), dimension(:,:,:) :: mag
+  real(dp), intent(inout), dimension(:,:,:) :: B
 
   component = ( &
-    sum(mag(1 : avg_shock_pos-cut-1, :, :)) + &
-    sum(mag(avg_shock_pos+cut+1 : nx, :, :)) &
+    sum(B(1 : avg_shock_pos-cut-1, :, :)) + &
+    sum(B(avg_shock_pos+cut+1 : nx, :, :)) &
   )/((nx-(2*cut-1)) * ny * nz)
 end subroutine make_cut_x
 
 
-subroutine make_cut_y(component, mag)
+subroutine make_cut_y(component, B)
   real(dp), intent(inout) :: component
-  real(dp), intent(inout), dimension(:,:,:) :: mag
+  real(dp), intent(inout), dimension(:,:,:) :: B
 
   component = ( &
-    sum(mag(:, 1 : avg_shock_pos-cut-1, :)) + &
-    sum(mag(:, avg_shock_pos+cut+1 : ny, :)) &
+    sum(B(:, 1 : avg_shock_pos-cut-1, :)) + &
+    sum(B(:, avg_shock_pos+cut+1 : ny, :)) &
   )/(nx * (ny-(2*cut-1)) * nz)
 end subroutine make_cut_y
 
 
-subroutine make_cut_z(component, mag)
+subroutine make_cut_z(component, B)
   real(dp), intent(inout) :: component
-  real(dp), intent(inout), dimension(:,:,:) :: mag
+  real(dp), intent(inout), dimension(:,:,:) :: B
 
   component = ( &
-    sum(mag(:, :, 1 : avg_shock_pos-cut-1)) + &
-    sum(mag(:, :, avg_shock_pos+cut+1 : nz)) &
+    sum(B(:, :, 1 : avg_shock_pos-cut-1)) + &
+    sum(B(:, :, avg_shock_pos+cut+1 : nz)) &
   )/(nx * ny * (nz-(2*cut-1)))
 end subroutine make_cut_z
 
 
-subroutine cut_average(component, mag)
+subroutine cut_average(component, B)
   real(dp), intent(inout) :: component
-  real(dp), intent(inout), dimension(:,:,:) :: mag
+  real(dp), intent(inout), dimension(:,:,:) :: B
 
-  if (shock_direction .eq. 'x') call make_cut_x(component, mag)
-  if (shock_direction .eq. 'y') call make_cut_y(component, mag)
-  if (shock_direction .eq. 'z') call make_cut_z(component, mag)    
+  if (shock_direction .eq. 'x') call make_cut_x(component, B)
+  if (shock_direction .eq. 'y') call make_cut_y(component, B)
+  if (shock_direction .eq. 'z') call make_cut_z(component, B)    
 end subroutine cut_average
 
 
-subroutine cut_delta_component(component, component_avg, mag)
+subroutine cut_delta_component(component, component_avg, B)
   real(dp), intent(inout) :: component
   real(dp), intent(inout) :: component_avg
-  real(dp), intent(inout), dimension(:,:,:) :: mag
+  real(dp), intent(inout), dimension(:,:,:) :: B
 
-  mag_temp = mag**2
-    if (shock_direction .eq. 'x') call make_cut_x(component_temp, mag_temp)
-    if (shock_direction .eq. 'y') call make_cut_y(component_temp, mag_temp)
-    if (shock_direction .eq. 'z') call make_cut_z(component_temp, mag_temp)    
+  B_temp = B**2
+    if (shock_direction .eq. 'x') call make_cut_x(component_temp, B_temp)
+    if (shock_direction .eq. 'y') call make_cut_y(component_temp, B_temp)
+    if (shock_direction .eq. 'z') call make_cut_z(component_temp, B_temp)    
   component = sqrt(component_temp - component_avg**2)
 
 end subroutine cut_delta_component
--- shock_tools.py
+++ shock_tools.py
@@ -9,112 +9,79 @@
 # The second averaging should ensure a smooth profile upstream of the shock.
 
 # Author: Michael Vorster
-# Last updated: 30 October 2017
+# Last updated: 3 August 2018
 
 
 import os
 import pyPLUTO as pp
 from fitcurve import savitzky_golay
-from numpy import (
-    argmax,
-    array,
-    average,
-    int_,
-    savetxt,
-    square,
-    sqrt,
-    transpose,
-    zeros
-)
-from matplotlib.pyplot import (
-    axis,
-    plot,
-    savefig,
-    show,
-    title,
-    xlabel,
-    ylabel
-)
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def create_scalar_fields(D):
-    density = D.rho
-    velocity = sqrt(
-        square(D.vx1) +
-        square(D.vx2) +
-        square(D.vx3)
-    )
-    magnetic_field = sqrt(
-        square(D.bx1) +
-        square(D.bx2) +
-        square(D.bx3)
-    )
+def plot_shock_plane(D, shock_index_plane, shock_direction, wdir, file_time):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
 
-    pressure = D.prs
-    # scalar_fields = [
-    #     density,
-    #     velocity,
-    #     magnetic_field
-    # ]
+    if shock_direction == 'x1':
+        X = np.arange(D.x2[0], D.x2[-1]+D.dx2[0], D.dx2[0])
+        Y = np.arange(D.x3[0], D.x3[-1]+D.dx3[0], D.dx3[0])
+        Z = np.array(shock_index_plane)/float(D.n1)
+        ax.set_xlabel('y')
+        ax.set_ylabel('z')
+        ax.set_zlabel('x')
+        ax.set_zlim(0, D.x1r[-1])
+    elif shock_direction == 'x2':
+        X = np.arange(D.x1[0], D.x1[-1]+D.dx1[0], D.dx1[0])
+        Y = np.arange(D.x3[0], D.x3[-1]+D.dx3[0], D.dx3[0])
+        Z = np.array(shock_index_plane)/float(D.n2)
+        ax.set_xlabel('x')
+        ax.set_ylabel('z')
+        ax.set_zlabel('y')
+        ax.set_zlim(0, D.x2r[-1])
+    else:
+        X = np.arange(D.x1[0], D.x1[-1]+D.dx1[0], D.dx1[0])
+        Y = np.arange(D.x2[0], D.x2[-1]+D.dx2[0], D.dx2[0])
+        Z = np.array(shock_index_plane)/float(D.n3)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        ax.set_zlim(0, D.x3r[-1])
 
-    # using just pressure for bx0.8_cs1.35 seems sufficient
-    scalar_fields = [
-        pressure
-    ]
-
-    return scalar_fields
-
-
-def locate_max_jump(variables):
-    avrg_index_variables = []
-    for variable in variables:
-        n = 10
-        dn = 0
-        dn_max = 100
-        initial_estimate = argmax(variable)
-        ratio = []
-        while dn < dn_max:
-            set_1 = variable[
-                initial_estimate - n + dn: initial_estimate + dn + 1
-            ]
-            set_2 = variable[
-                initial_estimate + dn + 1: initial_estimate + n + dn
-            ]
-            ratio.append(average(set_1)/average(set_2))
-            dn += 1
-        avrg_index_variables.append(
-            initial_estimate + argmax(ratio)
-        )
-    avrg_index = int(average(avrg_index_variables))
-
-    return avrg_index
+    X, Y = np.meshgrid(X, Y)
+    ax.plot_surface(X, Y, Z)
+    plt.savefig(wdir + 'shock_plane_' + str(file_time) + '.png')
+    # plt.show()
+    plt.clf()
 
 
-def locate_shock(D, shock_direction):
-    variables = create_scalar_fields(D)
-    nx = len(getattr(D, shock_direction))
+def locate_max_jump(pressure_slice, shock_pos_estimate):
+    di = shock_pos_estimate/2
+    i = shock_pos_estimate - di
 
-    # average over plane perpendicular to shock direction
-    avrg_variables = []
-    for variable in variables:
-        avrg_qx = [0]*nx
-        for index in range(0, nx):
-            if shock_direction == 'x1':
-                avrg_qx[index] = average(variable[index, :, :])
-            elif shock_direction == 'x2':
-                avrg_qx[index] = average(variable[:, index, :])
-            else:
-                avrg_qx[index] = average(variable[:, :, index])
-        avrg_variables.append(avrg_qx)
+    slice_avg = np.average(pressure_slice)
 
-    shock_index = locate_max_jump(avrg_variables)
+    while pressure_slice[i] > slice_avg:
+        i += 1
+
+    check_interval = range(i, i+di)
+    for j in check_interval:
+        if pressure_slice[j] > slice_avg:
+            i = j + 1
+
+    shock_index = i - 1
 
     return shock_index
 
 
-def locate_shock_plane(D, shock_direction, wdir, file_time):
-    variables = create_scalar_fields(D)
-
+def locate_shock_plane(
+    D,
+    shock_direction,
+    shock_pos_estimate,
+    wdir,
+    file_time
+):
     if shock_direction == 'x1':
         nx = [D.n2, D.n3]
     elif shock_direction == 'x2':
@@ -122,30 +89,33 @@ def locate_shock_plane(D, shock_direction, wdir, file_time):
     else:
         nx = [D.n1, D.n2]
 
-    shock_index_plane = zeros((nx[0], nx[1]), dtype=int_)
+    shock_index_plane = np.zeros((nx[0], nx[1]), dtype=np.int_)
 
     for n0 in range(0, nx[0]):
         for n1 in range(0, nx[1]):
-            variables_slices = []
-            for variable in variables:
-                if shock_direction == 'x1':
-                    single_variable_slice = variable[:, n0, n1]
-                elif shock_direction == 'x2':
-                    single_variable_slice = variable[n0, :, n1]
-                else:
-                    single_variable_slice = variable[n0, n1, :]
-                variables_slices.append(single_variable_slice)
+            if shock_direction == 'x1':
+                pressure_slice = D.prs[:, n0, n1]
+            elif shock_direction == 'x2':
+                pressure_slice = D.prs[n0, :, n1]
+            else:
+                pressure_slice = D.prs[n0, n1, :]
 
-            shock_index_plane[n0, n1] = locate_max_jump(variables_slices)
+            shock_index_plane[n0, n1] = locate_max_jump(
+                pressure_slice,
+                shock_pos_estimate
+            )
+            # shock_index_plane[n0, n1] = 246  # '+1' added few lines later
+
+    plot_shock_plane(D, shock_index_plane, shock_direction, wdir, file_time)
 
     # Fortran is column-major order, and Python is row-major order.
-    # Array must therefore be transposed for Fortran purposes.
-    # Should also add '1' to shock position since Fortran arrays start at 1
-    shock_index_plane_transposed = transpose(shock_index_plane+1)
+    # np.array must therefore be transposed for Fortran purposes.
+    # Should also add '1' to shock position since Fortran np.arrays start at 1
+    shock_index_plane_transposed = np.transpose(shock_index_plane+1)
     output_file_name = wdir + 'shock_plane_' + str(file_time) + '.txt'
-    savetxt(output_file_name, shock_index_plane_transposed)
+    np.savetxt(output_file_name, shock_index_plane_transposed)
 
-    return shock_index_plane
+    return np.average(shock_index_plane)
 
 
 def plot_average_fluid_field(
@@ -175,13 +145,13 @@ def plot_average_fluid_field(
     for char in ['$', '{', '}']:
         file_name = file_name.replace(char, '')
 
-    plot(x_grid, qx_slice, 'b', x_grid, avrg_qx, 'r', x_grid, avrg_fitted, 'k')
-    xlabel(shock_direction)
-    ylabel(r'Simulation [blue], Average [red], Fitted [black]')
-    title('Average ' + fluid_quantity)
-    axis([0, 1, y_min, y_max])
-    savefig(graph_dir + file_name + '_average')
-    show()
+    plt.plot(x_grid, qx_slice, 'b', x_grid, avrg_qx, 'r', x_grid, avrg_fitted, 'k')
+    plt.xlabel(shock_direction)
+    plt.ylabel(r'Simulation [blue], np.average [red], Fitted [black]')
+    plt.title('np.average ' + fluid_quantity)
+    plt.axis([0, 1, y_min, y_max])
+    plt.savefig(graph_dir + file_name + '_average')
+    plt.show()
 
 
 def average_fluid_field(
@@ -198,11 +168,11 @@ def average_fluid_field(
     avrg_qx = [0]*nx
     for index in range(0, nx):
         if shock_direction == 'x1':
-            avrg_qx[index] = average(qx[index, :, :])
+            avrg_qx[index] = np.average(qx[index, :, :])
         elif shock_direction == 'x2':
-            avrg_qx[index] = average(qx[:, index, :])
+            avrg_qx[index] = np.average(qx[:, index, :])
         else:
-            avrg_qx[index] = average(qx[:, :, index])
+            avrg_qx[index] = np.average(qx[:, :, index])
 
     avrg_fitted = list(avrg_qx)
 
@@ -210,11 +180,11 @@ def average_fluid_field(
         avrg_fitted[0:shock_index], 41, 1
     )
     if shock_direction == 'x1':
-        upstream_fit = average(qx[shock_index+1:, :, :])
+        upstream_fit = np.average(qx[shock_index+1:, :, :])
     elif shock_direction == 'x2':
-        upstream_fit = average(qx[:, shock_index+1:, :])
+        upstream_fit = np.average(qx[:, shock_index+1:, :])
     else:
-        upstream_fit = average(qx[:, :, shock_index+1:])
+        upstream_fit = np.average(qx[:, :, shock_index+1:])
 
     for index in range(0, shock_index):
         avrg_fitted[index] = downstream_fit[index]
@@ -262,7 +232,7 @@ def remove_average_fluid_component(
         qx_title = ['$V_{' + shock_direction + '}$']
         for perp_component in perp_components:
             vx = getattr(D, 'v' + perp_component)
-            del_qx.append(array(vx) - average(vx))
+            del_qx.append(np.array(vx) - np.average(vx))
     elif fluid_quantity == 'magnetic field':
         qx = [
             getattr(D, 'b' + perp_components[0]),
@@ -273,7 +243,7 @@ def remove_average_fluid_component(
             '$B_{' + perp_components[1] + '}$'
         ]
         bx_para = getattr(D, 'b' + shock_direction)
-        del_qx.append(array(bx_para) - average(bx_para))
+        del_qx.append(np.array(bx_para) - np.average(bx_para))
     else:
         print('Fluid quantity not recognised')
         exit(0)
@@ -292,18 +262,18 @@ def remove_average_fluid_component(
             plot_averages
         )
 
-        del_component = array(list(qx[component]))
+        del_component = np.array(list(qx[component]))
         for index in range(0, nx):
             if shock_direction == 'x1':
-                del_component[index, :, :] = array(
+                del_component[index, :, :] = np.array(
                     qx[component][index, :, :] - avrg_qx[index]
                 )
             elif shock_direction == 'x2':
-                del_component[:, index, :] = array(
+                del_component[:, index, :] = np.array(
                     qx[component][:, index, :] - avrg_qx[index]
                 )
             else:
-                del_component[:, :, index] = array(
+                del_component[:, :, index] = np.array(
                     qx[component][:, :, index] - avrg_qx[index]
                 )
         del_qx.append(del_component)
@@ -318,14 +288,10 @@ def remove_average_fluid_component(
 
 
 if __name__ == '__main__':
-    # wdir = '/home/mvorster/PLUTO/Shock_turbulence/Results/Run_15_b/'
-    # file_time = 7
-    # shock_direction = 'x1'
-    # shock_index = 535
-    wdir = '/home/cronus/vorster/PLUTO/512_Shock_turbulence/bx1.6_cs0.8/parallel_shock'
+    wdir = '/home/mvorster/512_cube/bx1.6_cs0.8/1_perpendicular_shock/Run_1/PLUTO'
     file_time = 4
-    shock_direction = 'x1'
-    # shock_index = 260
+    shock_direction = 'x2'
+    shock_pos_estimate = 256
 
     plot_averages = 0
 
@@ -334,8 +300,16 @@ if __name__ == '__main__':
     D = pp.pload(file_time, w_dir=wdir + 'output/')
     fluid_quantities = ['density', 'velocity', 'magnetic field']
 
-    shock_index = locate_shock_plane(D, shock_direction, wdir, file_time)
-    # print(shock_index)
+    shock_index = locate_shock_plane(
+      D,
+      shock_direction,
+      shock_pos_estimate,
+      wdir,
+      file_time
+    )
+
+    print(shock_index)
+
     # for fluid_quantity in fluid_quantities:
     #     remove_average_fluid_component(
     #         D,
